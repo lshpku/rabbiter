619ee7203feac7c0761cf32fad0f594cf97ac170
Liang Shuhao <liangshuhao@pku.edu.cn>
2020-02-12 19:16:09 +0800

    able to run dora

---
+++ dora.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+import tkinter.messagebox as mb
+from tkinter import ttk
+import xlrd
+from xlrd.sheet import Sheet
+import os
+import mailtable
+import utils
+from utils import Line, SheetSelector
+import webbrowser
+import threading
+
+
+window = tk.Tk()
+window.title('办公室报表工具')
+window.geometry('480x320')
+
+# file selector
+select_manifest = SheetSelector(window, '开票明细')
+select_clilist = SheetSelector(window, '客户分配表')
+
+
+def do_delivery():
+    d_s, d_a = does_sales.get(), does_annually.get()
+    try:
+        manifest = select_manifest.get()
+        assert manifest, '未选择开票明细'
+        clilist = select_clilist.get()
+        assert not (d_s and not clilist), (
+            '若要生成业务员统计，必须选择客户分配表')
+        assert d_s or d_a, '至少选择生成一项'
+        f1_label.set('转换中')
+        utils.log.clear()
+        mailtable.handle(manifest, clilist, d_s, d_a)
+        f1_label.set('转换完成')
+        utils.log.show()
+    except Exception as e:
+        f1_label.set('')
+        mb.showerror('错误', str(e))
+        return
+
+
+# checkbutton
+does_sales = tk.BooleanVar(value=True)
+tk.Checkbutton(window, text='生成业务员统计', variable=does_sales).pack()
+
+does_annually = tk.BooleanVar(value=False)
+tk.Checkbutton(window, text='生成年度统计', variable=does_annually).pack()
+
+
+tk.Button(window, text="转换", command=do_delivery).pack()
+
+f1_label = tk.StringVar()
+tk.Label(window, textvariable=f1_label).pack()
+
+ttk.Separator(window, orient='horizontal').pack(fill=tk.X)
+
+# author info
+with Line(window) as l:
+    tk.Label(l, text='项目地址：').pack(side='left')
+    url = tk.Label(l, text='https://github.com/lshpku/rabbiter')
+    url.pack(side='left')
+
+
+def open_url(event):
+    webbrowser.open('https://github.com/lshpku/rabbiter')
+
+
+url.bind("<Button-1>", open_url)
+
+window.mainloop()
--- mailtable.py
+++ mailtable.py
@@ -1,23 +1,72 @@
 import xlrd
 from xlrd.sheet import Sheet
 import xlwt
-from xlwt import Worksheet
+from xlwt import Worksheet, easyxf
 import os
 import utils
 from utils import AccountDatabase
 import time
+from typing import Dict
 
 ARGS = [
-    'DATE   CHAR(16) 1',   # 日期
-    'CLIENT CHAR(32) 4',   # 单位全名
-    'KIND   CHAR(16) 9',   # 产地
-    'WEIGHT REAL     10',  # 销售数量
+    'DATE   CHAR(16) 1',  # 日期
+    'CLIENT CHAR(32) 3',  # 单位全名
+    'KIND   CHAR(16) 5',  # 产地
+    'WEIGHT REAL     8',  # 销售数量
 ]
 
-ARGS_CLI = [
-    'SALES  CHAR(32) ',
-    'CLIENT CHAR(32) ',
-]
+KINDS = ['浓缩料', '乳猪料', '全价料', '禽料', '教槽', '预混料']
+EXPANDS = ['9018', '611']
+SALESMEN = '唐曾谢蒙廖易'
+HEADER = ['单位全名', '地区'] + KINDS + ['总计'] + EXPANDS
+
+BORDER_FULL = 'border: left thin, right thin, top thin, bottom thin;'
+HORZ_CENTER = 'align: horz center;'
+BKG_GREEN = 'pattern: pattern solid, fore_color light_green;'
+
+BORD = easyxf(BORDER_FULL)
+BORDC = easyxf(BORDER_FULL + HORZ_CENTER)
+UBORDC = easyxf(HORZ_CENTER)
+
+
+def write_sales(sheet: Worksheet, db: AccountDatabase,
+                sales: str, nrow: int) -> int:
+    ssum = [0] * (len(HEADER)-2)
+    for cli in db.sales_map[sales]:
+        sheet.write(nrow, 0, cli, BORD)
+        sheet.write(nrow, 1, sales, BORDC)
+
+        # write basic kinds
+        ksum = 0
+        for i, kind in enumerate(KINDS, 2):
+            whr = 'CLIENT={} AND KIND={}'.format(repr(cli), repr(kind))
+            val = db.select('SUM(WEIGHT)', whr).__next__()[0]
+            val = val/1000 if val else 0
+            ksum += val
+            ssum[i-2] += val
+            sheet.write(nrow, i, val if val else '', BORD)
+        sheet.write(nrow, i+1, ksum if ksum else '', BORD)
+        ssum[i-1] += ksum
+
+        # write extra kinds
+        for i, kind in enumerate(EXPANDS, i+2):
+            try:
+                kind = '{}.0'.format(int(kind))
+            except:
+                pass
+            whr = 'CLIENT={} AND KIND={}'.format(repr(cli), repr(kind))
+            val = db.select('SUM(WEIGHT)', whr).__next__()[0]
+            val = val/1000 if val else 0
+            ssum[i-2] += val
+            sheet.write(nrow, i, val if val else '', BORD)
+        nrow += 1
+
+    # write sales sum
+    sheet.write(nrow, 1, '合计', UBORDC)
+    for i, val in enumerate(ssum, 2):
+        sheet.write(nrow, i, val)
+    nrow += 2
+    return nrow
 
 
 def monthly(sheet: Worksheet, db: AccountDatabase):
@@ -26,24 +75,92 @@ def monthly(sheet: Worksheet, db: AccountDatabase):
     d = max([i[0] for i in db.distinct('DAY')])
     sheet.write(0, 0, '{}年每月份客户销量汇总表'.format(y))
     sheet.write(0, 3, '{}-{:02d}-{:02d}'.format(y, m, d))
-    pass
+
+    for i, j in enumerate(HEADER):
+        sheet.write(1, i, j, BORDC)
+        sheet.col(i).width_mismatch = True
+        sheet.col(i).width = 3400 if i < 1 else 2100
+    nrow = 2
+
+    for sal in db.sorted_one('SALES'):
+        nrow = write_sales(sheet, db, sal, nrow)
 
 
 def salesman(sheet: Worksheet, db: AccountDatabase):
     s = db.distinct('SALES').__next__()[0]
-    pass
+    clients = db.sorted_one('CLIENT')
+
+    for i, j in enumerate(HEADER):
+        sheet.write(0, i, j, BORDC)
+        sheet.col(i).width_mismatch = True
+        sheet.col(i).width = 3400 if i < 1 else 2100
+    nrow = 1
+
+    write_sales(sheet, db, s, nrow)
 
 
 def annually(sheet: Worksheet, db: AccountDatabase):
-    pass
+    sheet.write(0, 0, '', BORD)
+    header = HEADER[2:]
+    for i, h in enumerate(header, 1):
+        sheet.write(0, i, h, BORDC)
+    for i in range(len(header)+1):
+        sheet.col(i).width_mismatch = True
+        sheet.col(i).width = 2100
+
+    ssum = [0] * len(header)
+    for month in range(1, 13):
+        sheet.write(month, 0, '{}月'.format(month), BORD)
+        # write basic kinds
+        ksum = 0
+        for i, kind in enumerate(KINDS, 1):
+            whr = 'MONTH={} AND KIND={}'.format(month, repr(kind))
+            val = db.select('SUM(WEIGHT)', whr).__next__()[0]
+            val = val/1000 if val else 0
+            ksum += val
+            ssum[i-1] += val
+            sheet.write(month, i, val if val else '', BORD)
+        sheet.write(month, i+1, ksum if ksum else '', BORD)
+        ssum[i] += ksum
+
+        # write extra kinds
+        for i, kind in enumerate(EXPANDS, i+2):
+            try:
+                kind = '{}.0'.format(int(kind))
+            except:
+                pass
+            whr = 'MONTH={} AND KIND={}'.format(month, repr(kind))
+            val = db.select('SUM(WEIGHT)', whr).__next__()[0]
+            val = val/1000 if val else 0
+            ssum[i-1] += val
+            sheet.write(month, i, val if val else '', BORD)
+
+    # write col sum
+    sheet.write(13, 0, '合计', UBORDC)
+    for i, val in enumerate(ssum, 1):
+        sheet.write(13, i, val)
 
 
-def handle(sheet: Sheet, log=print):
-    db = AccountDatabase(sheet, ARGS, 10)
+def make_client_map(client_list: Sheet) -> Dict[str, str]:
+    clients = client_list.col_values(1, 1)
+    sales = client_list.col_values(3, 1)
+    client_map = {}
+    for i, client in enumerate(clients):
+        sal = sales[i]
+        ret = client_map.setdefault(client, sal)
+        if ret != sal:
+            e = '冲突：“{}”同时属于“{}”和“{}”'.format(client, ret, sal)
+            raise KeyError(e)
+    return client_map
+
+
+def handle(manifest: Sheet, client_list: Sheet,
+           does_sales=True, does_annually=False):
+    db = AccountDatabase(manifest, ARGS, 10)
     workbook = xlwt.Workbook(encoding='utf-8')
 
-    db.add_order('SALES', '唐曾谢蒙廖易')
-    db.add_order('KIND', ['浓缩料', '乳猪料', '全价料', '禽料', '教槽'])
+    db.add_order('SALES', SALESMEN)
+    db.add_order('KIND', KINDS)
 
     # add y, m, d to table
     dates = [i[0] for i in db.distinct('DATE')]
@@ -52,13 +169,30 @@ def handle(sheet: Sheet, log=print):
         t = time.strptime(i, '%Y-%m-%d')
         s = 'YEAR={}, MONTH={}, DAY={}'.format(t.tm_year, t.tm_mon, t.tm_mday)
         db.update(s, 'DATE={}'.format(repr(i)))
-    db.conn.commit()
+
+    # add sales to table
+    if does_sales:
+        db.add_colume('SALES CHAR(16)')
+        db.client_map = make_client_map(client_list)
+        clients = [i[0] for i in db.distinct('CLIENT')]
+        for cli in clients:
+            sal = db.client_map.get(cli)
+            assert sal, '客户“{}”无对应业务员'.format(cli)
+            db.update('SALES={}'.format(repr(sal)),
+                      'CLIENT={}'.format(repr(cli)))
+
+        db.sales_map = {}
+        for cli, sal in db.client_map.items():
+            db.sales_map.setdefault(sal, []).append(cli)
+        for clis in db.sales_map.values():
+            clis.sort()
 
     # write monthly
     years = db.sorted_one('YEAR')
     for y in years:
         months = db.sorted_one('MONTH', 'YEAR={}'.format(y))
         for m in months:
+            continue
             db.set_where('YEAR={} AND MONTH={}'.format(y, m))
             if len(years) > 1:  # multiple years
                 label = '{:02d}年{}月'.format(y % 100, m)
@@ -67,15 +201,17 @@ def handle(sheet: Sheet, log=print):
             monthly(workbook.add_sheet(label), db)
 
     # write salesmen
-    sales = db.sorted_one('SALES')
-    for s in sales:
-        db.set_where('SALES={}'.format(repr(s)))
-        salesman(workbook.add_sheet(s), db)
+    if does_sales:
+        sales = db.sorted_one('SALES')
+        for s in sales:
+            db.set_where('SALES={}'.format(repr(s)))
+            salesman(workbook.add_sheet(s), db)
 
     # write anually
-    for y in years:
-        db.set_where('YEAR={}'.format(y))
-        annually(workbook.add_sheet('{}年总合计'.format(y)), db)
+    if does_annually:
+        for y in years:
+            db.set_where('YEAR={}'.format(y))
+            annually(workbook.add_sheet('{}年总合计'.format(y)), db)
 
     cur = db.distinct('DAY', 'YEAR={} AND MONTH={}'.format(y, m))
     d = max([i[0] for i in cur])
@@ -85,5 +221,6 @@ def handle(sheet: Sheet, log=print):
 
 if __name__ == '__main__':
     workbook = xlrd.open_workbook(
-        'data/单位销售明细数据-2020_02_05-11_50_19.xls')
-    handle(workbook.sheet_by_index(0))
+        'data/单位销售明细数据-2020_02_11-16_39_46.xls')
+    client_list = xlrd.open_workbook('data/客户名.xls')
+    handle(workbook.sheet_by_index(0), client_list.sheet_by_index(0))
